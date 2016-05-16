368ed827d0151b086a2a78b7e7fff66e8d2e33f8
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-05-16 15:23:09 +0300

    Note for Indices

--- squash-core/src/org/jetbrains/squash/definition/ConstraintBuilder.kt
+++ squash-core/src/org/jetbrains/squash/definition/ConstraintBuilder.kt
@@ -12,6 +12,12 @@ fun <V> Column<V>.index(name: String? = null, unique: Boolean = false): Column<V
     table.addConstraint(IndexConstraint(Identifier(indexName), table, unique, listOf(this)))
 }
 
+// TODO: support full index column syntax, use builder like: index(name, unique).column(c1, ASC, NULLS_FIRST).column(c2, ...)
+// Do not allow empty index. Warn on NULLS order on NOT_NULL columns.
+//
+// H2: index_col_name: col_name [ASC | DESC] [NULLS (FIRST | LAST)]
+// MySQL: index_col_name: col_name [(length)] [ASC | DESC] ; # no NULLS order!
+// PgSQL: ( { column | ( expression ) } [ COLLATE collation ] [ opclass ] [ ASC | DESC ] [ NULLS { FIRST | LAST } ] [, ...] )
 fun Table.index(vararg columns: Column<*>, name: String? = null, unique: Boolean = false) {
     val indexName = name ?: "IX_${tableName.id}_${columns.joinToString("_") { it.name.id }}"
     addConstraint(IndexConstraint(Identifier(indexName), this, unique, columns.toList()))
