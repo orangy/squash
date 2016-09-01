b9d6fbc74d53dec9a9e50bf1c3f4926f19f2719d
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-09-01 20:00:27 +0300

    Unify typed data retrieval API

--- squash-core/src/org/jetbrains/squash/results/ResponseRow.kt
+++ squash-core/src/org/jetbrains/squash/results/ResponseRow.kt
@@ -9,22 +9,21 @@ import kotlin.reflect.*
  */
 interface ResponseRow {
     /**
-     * Gets value of a column with the give [type] and [columnName]
+     * Gets value of a column with the given [type], [columnName] and optional [tableName]
      */
     fun <V> columnValue(type: KClass<*>, columnName: String, tableName: String? = null): V
 
     /**
-     * Gets value of a column with the give [type] and [index]
+     * Gets value of a column with the given [type] and [index]
      */
     fun <V> columnValue(type: KClass<*>, index: Int): V
 }
 
 operator inline fun <reified V> ResponseRow.get(name: String): V = columnValue(V::class, name)
 operator inline fun <reified V> ResponseRow.get(index: Int): V = columnValue(V::class, index)
+operator inline fun <reified V> ResponseRow.get(column: Column<V>): V = columnValue(V::class, column)
 
-inline fun <reified V> ResponseRow.columnValue(column: Column<V>): V {
+fun <V> ResponseRow.columnValue(type: KClass<*>, column: Column<V>): V {
     val label = if (column is AliasColumn) column.label.id else column.name.id
-    return columnValue(V::class, label, column.table.tableName.id)
+    return columnValue(type, label, column.table.tableName.id)
 }
-
-operator inline fun <reified V> ResponseRow.get(column: Column<V>): V = columnValue(column)
