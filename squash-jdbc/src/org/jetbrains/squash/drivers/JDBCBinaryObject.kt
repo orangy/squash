b624407c60d86759f33e5e9755aba75d901c9b1e
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-10-24 14:46:08 +0300

    Add diagnostic toString for JDBC blob

--- squash-jdbc/src/org/jetbrains/squash/drivers/JDBCBinaryObject.kt
+++ squash-jdbc/src/org/jetbrains/squash/drivers/JDBCBinaryObject.kt
@@ -2,4 +2,6 @@ package org.jetbrains.squash.drivers
 
 import org.jetbrains.squash.connection.*
 
-class JDBCBinaryObject(override val bytes: ByteArray) : BinaryObject
+class JDBCBinaryObject(override val bytes: ByteArray) : BinaryObject {
+    override fun toString(): String = "BLOB(${bytes.size}"
+}
