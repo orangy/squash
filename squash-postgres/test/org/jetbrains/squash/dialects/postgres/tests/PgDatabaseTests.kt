bf6fa17fd7252598481cf90e97998ef58d834cac
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-10-26 15:32:13 +0300

    Use embedded postgres for testing

--- squash-postgres/test/org/jetbrains/squash/dialects/postgres/tests/PgDatabaseTests.kt
+++ squash-postgres/test/org/jetbrains/squash/dialects/postgres/tests/PgDatabaseTests.kt
@@ -1,10 +1,13 @@
 package org.jetbrains.squash.dialects.postgres.tests
 
+import com.opentable.db.postgres.embedded.*
 import kotlinx.support.jdk7.*
 import org.jetbrains.squash.connection.*
 import org.jetbrains.squash.definition.*
 import org.jetbrains.squash.dialects.postgres.*
 import org.jetbrains.squash.tests.*
+
+val pg = EmbeddedPostgres.builder().start()
 
 class PgDatabaseTests : DatabaseTests {
     override val idColumnType: String get() = "SERIAL"
@@ -12,7 +15,7 @@ class PgDatabaseTests : DatabaseTests {
     override fun autoPrimaryKey(table: String, column: String): String = primaryKey(table, column)
 
     fun <R> withConnection(block: (DatabaseConnection) -> R): R {
-        val connection = PgConnection.create("localhost:5432/", "postgres")
+        val connection = PgConnection.create("localhost:${pg.port}/", "postgres")
         return block(connection)
     }
 
