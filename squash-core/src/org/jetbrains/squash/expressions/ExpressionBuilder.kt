a74807498d782d973ba382402241e9dd11cec9ad
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-11-22 17:28:37 +0300

    Add proper overloads to gt[eq], lt[eq] with expression and literals

--- squash-core/src/org/jetbrains/squash/expressions/ExpressionBuilder.kt
+++ squash-core/src/org/jetbrains/squash/expressions/ExpressionBuilder.kt
@@ -4,9 +4,6 @@ import org.jetbrains.squash.query.*
 import org.jetbrains.squash.statements.*
 import kotlin.internal.*
 
-infix fun <V> Expression<@Exact V?>.eq(other: Expression<V?>): Expression<Boolean> = EqExpression(this, other)
-infix fun <V> Expression<@Exact V>.eq(literal: V): Expression<Boolean> = EqExpression(this, LiteralExpression(literal))
-
 infix fun Expression<Boolean>.and(other: Expression<Boolean>): Expression<Boolean> = AndExpression(this, other)
 infix fun Expression<Boolean>.or(other: Expression<Boolean>): Expression<Boolean> = OrExpression(this, other)
 
@@ -14,13 +11,22 @@ infix fun Expression<Boolean>.or(other: Expression<Boolean>): Expression<Boolean
 fun Expression<Boolean>.not(): Expression<Boolean> = NotExpression(this)
 fun not(expression: Expression<Boolean>): Expression<Boolean> = NotExpression(expression)
 
+infix fun <V> Expression<@Exact V?>.eq(other: Expression<V?>): Expression<Boolean> = EqExpression(this, other)
+infix fun <V> Expression<@Exact V>.eq(literal: V): Expression<Boolean> = EqExpression(this, LiteralExpression(literal))
+
 infix fun <V> Expression<@Exact V>.neq(other: Expression<V>): Expression<Boolean> = NotEqExpression(this, other)
 infix fun <V> Expression<@Exact V>.neq(literal: V): Expression<Boolean> = NotEqExpression(this, LiteralExpression(literal))
 
 infix fun <V> Expression<@Exact V>.lt(other: Expression<V>): Expression<Boolean> = LessExpression(this, other)
-infix fun <V> Expression<@Exact V>.lteq(other: Expression<V>): Expression<Boolean> = LessEqExpression(this, other)
+infix fun <V> Expression<@Exact V>.lt(literal: V): Expression<Boolean> = LessExpression(this, LiteralExpression(literal))
 
+infix fun <V> Expression<@Exact V>.lteq(other: Expression<V>): Expression<Boolean> = LessEqExpression(this, other)
+infix fun <V> Expression<@Exact V>.lteq(literal: V): Expression<Boolean> = LessEqExpression(this, LiteralExpression(literal))
+
+infix fun <V> Expression<@Exact V>.gt(other: Expression<V>): Expression<Boolean> = GreaterExpression(this, other)
 infix fun <V> Expression<@Exact V>.gt(literal: V): Expression<Boolean> = GreaterExpression(this, LiteralExpression(literal))
+
+infix fun <V> Expression<@Exact V>.gteq(other: Expression<V>): Expression<Boolean> = GreaterEqExpression(this, other)
 infix fun <V> Expression<@Exact V>.gteq(literal: V): Expression<Boolean> = GreaterEqExpression(this, LiteralExpression(literal))
 
 infix operator fun <V> Expression<@Exact V>.plus(other: Expression<V>): Expression<V> = PlusExpression(this, other)
