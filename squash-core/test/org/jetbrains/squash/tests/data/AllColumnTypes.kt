58dde89dd2f2ace6dc34a0911be90acf5513777a
Mike Kelp <mikekelp@gmail.com>
2019-05-20 10:17:32 -0700

    Testing : Created withAllColumnTypes() method similar to withCities()
    This is for other dialects to use the AllColumnTypes table for their unit tests.

--- squash-core/test/org/jetbrains/squash/tests/data/AllColumnTypes.kt
+++ squash-core/test/org/jetbrains/squash/tests/data/AllColumnTypes.kt
@@ -1,6 +1,16 @@
 package org.jetbrains.squash.tests.data
 
+import org.jetbrains.squash.connection.BinaryObject
+import org.jetbrains.squash.connection.Transaction
 import org.jetbrains.squash.definition.*
+import org.jetbrains.squash.statements.insertInto
+import org.jetbrains.squash.statements.values
+import org.jetbrains.squash.tests.DatabaseTests
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.util.*
 
 enum class E {
     ONE,
@@ -22,4 +32,25 @@ object AllColumnTypes : TableDefinition() {
     val binary = binary("binary", 128)
     val blob = blob("blob")
     val uuid = uuid("uuid")
+}
+
+fun <R> DatabaseTests.withAllColumnTypes(statement:Transaction.() -> R) :R {
+	return withTables(AllColumnTypes) {
+		insertInto(AllColumnTypes).values {
+			it[varchar] = "varchar"
+			it[char] = "c"
+			it[enum] = E.ONE
+			it[decimal] = BigDecimal.ONE
+			it[long] = 222L
+			it[date] = LocalDate.of(1976, 11, 24)
+			it[bool] = true
+			it[datetime] = LocalDateTime.of(LocalDate.of(1976, 11, 24), LocalTime.of(8, 22))
+			it[text] = "Lorem Ipsum"
+			it[binary] = byteArrayOf(1, 2, 3)
+			it[blob] = BinaryObject.fromByteArray(this@withTables, byteArrayOf(1, 2, 3))
+			it[uuid] = UUID.fromString("7cb64fe4-4938-4e88-8d94-17e929d40c99")
+		}.execute()
+		
+		statement()
+	}
 }
